e5f6b59d91c7e50a9fd52506cf6a048ffbdd6294
Sumit Gautam <sghost33@gmail.com>
2021-06-22 21:53:23 +1000

    Add card view over to markdown editor and preview

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -3,13 +3,14 @@ import MDEditor from '@uiw/react-md-editor';
 import { useState, useEffect, useRef } from 'react';
 
 const TextEditor: React.FC = () => {
-	const [ editing, setEditing ] = useState(false);
 	const ref = useRef<HTMLDivElement | null>(null);
+	const [ editing, setEditing ] = useState(false);
+	const [ value, setValue ] = useState('# Header');
 
 	useEffect(() => {
 		const listener = (event: MouseEvent) => {
 			if (ref.current && event.target && ref.current.contains(event.target as Node)) {
-        return;
+				return;
 			}
 			setEditing(false);
 		};
@@ -23,14 +24,16 @@ const TextEditor: React.FC = () => {
 	if (editing) {
 		return (
 			<div className="text-editor" ref={ref}>
-				<MDEditor />
+				<MDEditor value={value} onChange={(v) => setValue(v || '')} />
 			</div>
 		);
 	}
 
 	return (
-		<div className="text-editor" onClick={() => setEditing(true)}>
-			<MDEditor.Markdown source={'# Header'} />
+		<div onClick={() => setEditing(true)}>
+			<div className="card-content">
+				<MDEditor.Markdown source={value} />
+			</div>
 		</div>
 	);
 };
