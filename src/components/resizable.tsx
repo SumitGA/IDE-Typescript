61bdbac8e1d51e4a82d68c253eb266ad11e18e81
Sumit Gautam <sghost33@gmail.com>
2021-06-22 15:03:20 +1000

    Fix resizing pane from resetting to orignal state with use of state and width

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -8,21 +8,30 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 	let resizableProps: ResizableBoxProps;
-  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+	const [ innerHeight, setInnerHeight ] = useState(window.innerHeight);
+	const [ innerWidth, setInnerWidth ] = useState(window.innerWidth);
+	const [ width, setWidth ] = useState(window.innerWidth * 0.75);
 
-  useEffect(() => {
-    const listener = () => {
-      setInnerHeight(window.innerHeight);
-      setInnerWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', listener);
+	useEffect(() => {
+		let timer: any;
+		const listener = () => {
+			if (timer) {
+				clearTimeout(timer);
+			}
+			setTimeout(() => {
+				setInnerHeight(window.innerHeight);
+				setInnerWidth(window.innerWidth);
+				if (window.innerWidth * 0.75 < width) {
+					setWidth(window.innerWidth * 0.75);
+				}
+			}, 100);
+		};
+		window.addEventListener('resize', listener);
 
-    return () => {
-      window.removeEventListener('resize', listener);
-    }
-  }, [])
-
+		return () => {
+			window.removeEventListener('resize', listener);
+		};
+	}, [width]);
 
 	if (direction === 'horizontal') {
 		resizableProps = {
@@ -30,8 +39,11 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 			minConstraints: [ innerWidth * 0.2, Infinity ],
 			maxConstraints: [ innerWidth * 0.75, Infinity ],
 			height: Infinity,
-			width: innerWidth * 0.75,
-			resizeHandles: [ 'e' ]
+			width,
+			resizeHandles: [ 'e' ],
+			onResizeStop: (event, data) => {
+				setWidth(data.size.width);
+			}
 		};
 	} else {
 		resizableProps = {
