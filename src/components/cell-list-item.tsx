592bd84b0556b7da680254777ec37785c998bcc7
Sumit Gautam <sghost33@gmail.com>
2021-06-27 15:52:16 +1000

    Add CellList item and creating useSelector to retains order and data from state

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,1 +1,11 @@
-export default 1;
+import { Cell } from '../state';
+
+interface CellListItemProps {
+	cell: Cell;
+}
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+	return <div>{cell.id}</div>;
+};
+
+export default CellListItem;
--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,8 +1,11 @@
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
 
 const CellList: React.FC = () => {
-  useTypedSelector((state) => state);
-	return <div>Cell List</div>;
+	const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+	const renderCells = cells.map((cell) => <CellListItem key={cell.id} cell={cell} />);
+
+	return <div>{renderCells}</div>;
 };
 
 export default CellList;
