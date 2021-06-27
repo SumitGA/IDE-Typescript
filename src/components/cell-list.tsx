bfd6199b23d0941026d5d5ecb79debd40442a093
Sumit Gautam <sghost33@gmail.com>
2021-06-27 14:41:04 +1000

    add useSelector hook and export it to globally to use 'useSelector hook' for react FC

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 const CellList: React.FC = () => {
-  return <div>Cell List</div>
+  useTypedSelector((state) => state);
+	return <div>Cell List</div>;
 };
 
 export default CellList;
