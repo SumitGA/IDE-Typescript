7f3c6013c04bb60159c3b705a3b614eb29445b01
Sumit Gautam <sghost33@gmail.com>
2021-06-28 22:14:54 +1000

    Add visibility incase we do not have any cell

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -5,17 +5,21 @@ import AddCell from './add-cell';
 
 const CellList: React.FC = () => {
 	const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
-	const renderCells = cells.map((cell) => 
+	const renderCells = cells.map((cell) => (
 		<Fragment key={cell.id}>
-			<AddCell nextCellId={cell.id}/>
+			<AddCell nextCellId={cell.id} />
 			<CellListItem cell={cell} />
 		</Fragment>
-	);
+	));
 
-	return <div>
-		{renderCells}
-			<AddCell nextCellId={null}/>
-		</div>;
+	return (
+		<div>
+			{renderCells}
+			<div className={cells.length === 0 ? 'force-visible' : ''}>
+				<AddCell nextCellId={null} />
+			</div>
+		</div>
+	);
 };
 
 export default CellList;
