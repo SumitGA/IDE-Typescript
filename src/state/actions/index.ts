1bf6d93d5628ceeebb385722989697423ac8b721
Sumit Gautam <sghost33@gmail.com>
2021-06-29 15:56:02 +1000

    Add bundle_start and bundle_complete action types

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -26,8 +26,26 @@ export interface InsertCellAfterAction {
 export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
-    id: string,
+    id: string;
     content: string;
+  }
+}
+
+export interface BundleStartAction {
+  type: ActionType.BUNDLE_START;
+  payload: {
+    cellId: string;
+  }
+}
+
+export interface BundleCompleteAction {
+  type: ActionType.BUNDLE_COMPLETE;
+  payload: {
+    cellId: string;
+    bundle: {
+      code: string;
+      err: string;
+    }
   }
 }
 
@@ -35,4 +53,6 @@ export type Action =
   | MoveCellAction
   | DeleteCellAction
   | InsertCellAfterAction
-  | UpdateCellAction;
+  | UpdateCellAction
+  | BundleStartAction
+  | BundleCompleteAction;
