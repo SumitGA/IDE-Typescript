92321345bf03fd5aedf26886460978bf9b3bd663
Sumit Gautam <sghost33@gmail.com>
2021-06-29 16:56:22 +1000

    setting up eager loading for bundle and also checking if bundle is already done

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,6 +16,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
 	useEffect(
 		() => {
+			if (!bundle) {
+				createBundle(cell.id, cell.content);
+				return;
+			}
 			const timer = setTimeout(async () => {
 				createBundle(cell.id, cell.content);
 			}, 750);
@@ -24,6 +28,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 				clearTimeout(timer);
 			};
 		},
+		//	eslint-disable-next-line react-hooks/exhaustive-deps
 		[ cell.content, cell.id, createBundle ]
 	);
 
