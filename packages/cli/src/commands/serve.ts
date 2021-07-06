f15d74235eba2e0dbbe4e4d69533dd4d39217d0e
Sumit Gautam <sghost33@gmail.com>
2021-07-06 12:48:01 +1000

    Add local-client dependency to local-api and decide to use proxy or server static page

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,23 @@
 import { Command } from 'commander';
+import { serve } from 'local-api';
+import path from 'path';
+import { exit } from 'process';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options) => {
-    console.log(filename, options);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename))
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit this file`);
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.log('Port is in use. Try running on different port.')
+      } else {
+        console.log('Heres the problem', err.message);
+      }
+      process.exit(1);
+    }
   });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,22 @@
-export default () => {
-  console.log('Server is listeninig');
-}
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+
+export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
+  const app = express();
+  const packagePath = require.resolve('local-client/build/index.html');
+
+  if (useProxy) {
+    app.use(createProxyMiddleware({
+      target: 'http://localhost:3000',
+      ws: true,
+      logLevel: 'silent'
+    }));
+  } else {
+    app.use(express.static(path.dirname(packagePath)));
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  })
+};
